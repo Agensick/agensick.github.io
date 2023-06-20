08007015eb5f2d425b259605567379446ad30e63
LoboGuardian <30099451+LoboGuardian@users.noreply.github.com>
2023-06-19 23:28:58 -0400

    update header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,11 @@
-document.querySelector("button.button-menu-toggle")
-.addEventListener("click", function() {
-document.querySelector(".nav-links").
-classList.toggle("nav-links-responsive")})
+$(document).ready(function(){
 
-const miBoton = document.getElementById('miBoton');
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('header').addClass('header2');
+		} else {
+			$('header').removeClass('header2');
+		}
+	});
 
-function handleClick() {
-     alert('Hola!, has hecho clic en el botón.');
-    }
-
-miBoton.addEventListener("click", handleClick)
-
-function showAlert() {
-     alert('ALERTA');
- }
-
-const form = document.getElementById('myForm');
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  validateForm();
 });
-
-function validateEmail(email) {
-    const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
-    return regex.test(email)
-  }
-  
-  function validateForm() {
-    const emailInput = document.getElementById('email');
-    const email = emailInput.value;
-  
-    if (!validateEmail(email)) {
-      alert('Por favor ingrese un correo electrónico válido.');
-    } else {
-      alert('Correo electrónico enviado correctamente.');
-    }
-  }
